9fd5b2a2097b07e478313412d5daeeb0c298955b
Harsh Kumar Nayak <KIIT@BT-2128021.(none)>
2024-07-07 23:06:52 +0530

    Fixed issues with imports and environment variables

--- src/routes/profiles/+page.server.ts
+++ src/routes/profiles/+page.server.ts
@@ -1,14 +1,13 @@
 import { createPool } from '@vercel/postgres';
-import { POSTGRES_URL } from '$env/static/private';
+import { POSTGRES_URL } from '$env/static/private'; // Ensure this is correctly set in your environment variables
 
 export async function load() {
-  console.log('POSTGRES_URL:', POSTGRES_URL); // Debug log to verify
   const db = createPool({ connectionString: POSTGRES_URL });
 
   try {
     const { rows: names } = await db.query('SELECT * FROM names');
     return {
-      names: names,
+      names,
     };
   } catch (error) {
     console.log('Table does not exist, creating and seeding it with dummy data now...');
@@ -23,31 +22,33 @@ export async function load() {
 async function seed() {
   const db = createPool({ connectionString: POSTGRES_URL });
   const client = await db.connect();
-  await client.sql`CREATE TABLE IF NOT EXISTS names (
+  await client.sql`
+    CREATE TABLE IF NOT EXISTS names (
       id SERIAL PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
       email VARCHAR(255) UNIQUE NOT NULL,
       "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-    );`;
+    );
+  `;
 
   console.log(`Created "users" table`);
 
   const users = await Promise.all([
     client.sql`
-          INSERT INTO names (name, email)
-          VALUES ('Rohan', 'rohan@tcl.com')
-          ON CONFLICT (email) DO NOTHING;
-      `,
+      INSERT INTO names (name, email)
+      VALUES ('Rohan', 'rohan@tcl.com')
+      ON CONFLICT (email) DO NOTHING;
+    `,
     client.sql`
-          INSERT INTO names (name, email)
-          VALUES ('Rebecca', 'rebecca@tcl.com')
-          ON CONFLICT (email) DO NOTHING;
-      `,
+      INSERT INTO names (name, email)
+      VALUES ('Rebecca', 'rebecca@tcl.com')
+      ON CONFLICT (email) DO NOTHING;
+    `,
     client.sql`
-          INSERT INTO names (name, email)
-          VALUES ('Vivek', 'vivek@gmail.com')
-          ON CONFLICT (email) DO NOTHING;
-      `,
+      INSERT INTO names (name, email)
+      VALUES ('Vivek', 'vivek@gmail.com')
+      ON CONFLICT (email) DO NOTHING;
+    `,
   ]);
   console.log(`Seeded ${users.length} users`);
 
@@ -67,8 +68,9 @@ export const actions = {
     const id = data.get('id');
 
     await client.sql`
-    DELETE FROM names
-    WHERE id = ${id};`;
+      DELETE FROM names
+      WHERE id = ${id};
+    `;
 
     return { success: true };
   },
